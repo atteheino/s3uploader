e54032210116d5d64bbf59a3d8851b0007e731ae
atteheino <atte.heino@gmail.com>
2018-04-14 20:54:04 +0300

    Simple handler for filesystem events

---
+++ src/filewatch_handler.py
@@ -0,0 +1,25 @@
+from watchdog.events import LoggingEventHandler
+from watchdog.events import FileSystemEventHandler
+
+class MyHandler(FileSystemEventHandler):
+
+    def process(self, event):
+        """
+        event.event_type
+            'modified' | 'created' | 'moved' | 'deleted'
+        event.is_directory
+            True | False
+        event.src_path
+            path/to/observed/file
+        """
+
+        print(event.file)
+        print(event.is_directory)
+        print(event.event_type)
+
+    def on_modified(self, event):
+        self.process(event)
+
+    def on_created(self, event):
+        self.process(event)
+
