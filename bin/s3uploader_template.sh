99591e681ea8ad7ebd30611c206a31583474c578
atteheino <atte.heino@gmail.com>
2018-07-09 22:16:44 +0300

    Run in the background

--- bin/s3uploader_template.sh
+++ bin/s3uploader_template.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-python3 ../src/main.py -d /var/ftp/videoftp/files -p <profile> -b <bucket> -i 60 start
+nohup python3 ../src/main.py -d /var/ftp/videoftp/files -p <profile> -b <bucket> -i 60 start &
